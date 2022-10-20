ca10a277ef018cc279603c1714d7db3dfb855184
Samyusuf101 <samyusuf101@gmail.com>
2022-10-20 14:00:42 -0700

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,35 +7,34 @@ import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 import Projects from "../components/Projects";
 import Skills from "../components/Skills";
-import styles from "../styles/styles";
 
 const Home = () => (
   <div
     className="bg-[#242424] w-full h-screen  overflow-x-hidden snap-y snap-mandatory 
   overflow-y-scroll z-0"
   >
-    <div className={`${styles.paddingX} ${styles.flexCenter}`}>
-      <div className={`${styles.boxWidth}`}>
+    <div className="first-letter:">
+      <div className="">
         <Navbar />
       </div>
     </div>
     <section id="hero" className=" text-white snap-start">
-      <div className={`bg-[#242424] ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
+      <div className="">
+        <div className="">
           <Hero />
         </div>
       </div>
     </section>
     <section id="about" className=" text-white snap-center">
-      <div className={`bg-[#242424] ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
+      <div className="first-letter:">
+        <div className="">
           <About />
         </div>
       </div>
     </section>
     <section id="skills" className=" text-white snap-center ">
-      <div className={`bg-[#242424] ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
+      <div className="">
+        <div className="">
           <Skills />
         </div>
       </div>
@@ -46,14 +45,14 @@ const Home = () => (
       className="relative mt-8 pt-100 py-80 md:pt-5  text-white snap-start "
     >
       <div className={`bg-[#242424] `}>
-        <div className={`${styles.boxWidth}`}>
+        <div className="">
           <Projects />
         </div>
       </div>
     </section>
     <section id="contacts" className=" text-white snap-center ">
-      <div className={`bg-[#242424] ${styles.flexStart}`}>
-        <div className={`${styles.boxWidth}`}>
+      <div className="">
+        <div className="">
           <Contact />
         </div>
       </div>
