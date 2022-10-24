89a4612e3be2524a4006731484bfbfb15ed999be
Samyusuf101 <samyusuf101@gmail.com>
2022-10-24 09:11:59 -0700

    updated skills

--- pages/Projects.tsx
+++ pages/Projects.tsx
@@ -16,18 +16,14 @@ function Projects({}: Props) {
     items-center px-10 text-left overflow-x-hidden md:flex-row  
     !scrollbar-thin !scrollbar-track-transparent !scrollbar-thumb-red-600 "
     >
-      <h3
-        className="absolute top-28 pb-4 uppercase tracking-[20px] text-white text-2xl 
-      "
-      >
+      <h3 className="absolute top-28 pb-4 uppercase tracking-[20px] text-white text-2xl ">
         Projects
       </h3>
 
       <div className="relative mt-80 pt-40 top-20 lg:mt-60 lg:pt-80  md:pt-60 md:mt-60  grid md:grid-cols-2 gap-8">
         <div
           className="relative flex flex-col items-center justify-center h-auto w-full pt-25
-     rounde-xl p-4 group hover:scale-105 transition transformation duration-200
-    ease-out"
+     rounde-xl p-4 "
         >
           <Image
             className="rounded-xl group-hover:opacity-10"
@@ -38,8 +34,7 @@ function Projects({}: Props) {
         </div>
         <div
           className="relative flex flex-col items-center justify-center h-auto w-full pt-15
-     rounde-xl p-4 group hover:scale-105 transition transformation duration-200
-    ease-out"
+     rounde-xl p-4 "
         >
           <Image
             className="rounded-xl group-hover:opacity-10"
@@ -50,8 +45,7 @@ function Projects({}: Props) {
         </div>
         <div
           className="relative flex flex-col items-center justify-center h-auto w-full pt-15
-     rounde-xl p-4 group hover:scale-105 transition transformation duration-200
-    ease-out"
+     rounde-xl p-4 "
         >
           <Image
             className="rounded-xl group-hover:opacity-10"
@@ -62,8 +56,7 @@ function Projects({}: Props) {
         </div>
         <div
           className="relative flex flex-col items-center justify-center cursor-pointer h-auto w-full 
-     rounde-xl p-4 pt-15 hover:scale-105 transition transformation duration-200
-     ease-out"
+     rounde-xl p-4 pt-15 hover:scale-105 transition transformation duration-200"
         >
           <Image
             className="rounded-xl group-hover:opacity-10"
