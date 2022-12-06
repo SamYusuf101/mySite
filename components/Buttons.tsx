cd256dd38c727d29a5da9099e755b5651b4385ff
Sam Yusuf <samyusuf101@gmail.com>
2022-12-06 21:51:19 +0100

    Update Buttons.tsx

--- components/Buttons.tsx
+++ components/Buttons.tsx
@@ -13,7 +13,7 @@ import Link from "next/link";
   <Link href="/Contact">
     <button
       className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold 
-         hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+         hover:text-white py-3 px-4 border border-blue-500 hover:border-transparent rounded"
     >
       Button
     </button>
