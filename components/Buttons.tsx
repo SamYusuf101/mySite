e20f7a26bda591c172351cac2af193d52666196c
Sam Yusuf <samyusuf101@gmail.com>
2022-12-13 21:58:33 +0100

    Update Buttons.tsx

--- components/Buttons.tsx
+++ components/Buttons.tsx
@@ -3,11 +3,7 @@ import Link from "next/link";
 <div className="space-x-4 space-y-4 pt-80  mt-60 md:pt-80 md:mt-10 lg:pt-60  ">
   
   <Link href="/Skills">
-    <button
-      className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold 
-         hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
-    >
-      Button
+    
     </button>
   </Link>
 Link>
