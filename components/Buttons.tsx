8f932aa8c7f12d1c961d202f5561e0c8d9407256
Sam Yusuf <samyusuf101@gmail.com>
2023-01-07 22:09:45 +0100

    Update Buttons.tsx

--- components/Buttons.tsx
+++ components/Buttons.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 
-<div className="space-x-4 space-y-5 pt-80  mt-60 md:pt-80 md:mt-10 lg:pt-60  ">
+<div className="space-x-5 space-y-5 pt-80  mt-60 md:pt-80 md:mt-10 lg:pt-60  ">
   
   
     <button>
