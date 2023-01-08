abc64747ad28566afdb269fb8e4c274c32116dd1
Sam Yusuf <samyusuf101@gmail.com>
2023-01-08 20:32:03 +0100

    Update BackgroundCircle.tsx

--- components/BackgroundCircle.tsx
+++ components/BackgroundCircle.tsx
@@ -8,7 +8,7 @@ function BackgroundCircle({}: Props) {
     <div className="flex justify-center mb-12 items-center relative">
       <div className="border absolute border-white rounded-full h-[150px] w-[150px] mt-52 animate-ping" />
       <div className="border absolute border-white opacity-25 rounded-full h-[250px] w-[250px] mt-52 " />
-      <div className="border absolute border-red-500 opacity-20 rounded-full h-[450px] w-[450px] mt-52 animate-pulse" />
+      <div className="border absolute border-red-600 opacity-20 rounded-full h-[450px] w-[450px] mt-52 animate-pulse" />
 
       <div />
     </div>
