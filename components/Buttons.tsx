ace8f08467d6c4a38391065b802dcdc25f8c310c
Sam Yusuf <samyusuf101@gmail.com>
2023-01-12 20:58:21 +0100

    Update Buttons.tsx

--- components/Buttons.tsx
+++ components/Buttons.tsx
@@ -7,6 +7,7 @@ import Link from "next/link";
   </Link>
 Link>
 </div>
+
 <div className="space-x-5 space-y-5 pt-80  mt-60 md:pt-80 md:mt-10 lg:pt-60  ">
     <button>
     </button>
@@ -55,5 +56,34 @@ Link>
 
   </Link>
 Link>
-</div>;
+</div>
+<div className="space-x-5 space-y-5 pt-80  mt-60 md:pt-80 md:mt-10 lg:pt-60  ">
+    <button>
+    </button>
+
+  </Link>
+Link>
+</div>
+<div className="space-x-5 space-y-5 pt-80  mt-60 md:pt-80 md:mt-10 lg:pt-60  ">
+    <button>
+    </button>
+
+  </Link>
+Link>
+</div>
+<div className="space-x-5 space-y-5 pt-80  mt-60 md:pt-80 md:mt-10 lg:pt-60  ">
+    <button>
+    </button>
+
+  </Link>
+Link>
+</div>
+<div className="space-x-5 space-y-5 pt-80  mt-60 md:pt-80 md:mt-10 lg:pt-60  ">
+    <button>
+    </button>
+
+  </Link>
+Link>
+</div>
+
 
