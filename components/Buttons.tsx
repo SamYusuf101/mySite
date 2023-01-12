a4f414881d0fdd7b08ab002c5647b43e82dec6bb
Sam Yusuf <samyusuf101@gmail.com>
2023-01-12 21:12:04 +0100

    Update Buttons.tsx

--- components/Buttons.tsx
+++ components/Buttons.tsx
@@ -14,6 +14,20 @@ Link>
   </Link>
 Link>
 </div>
+<div className="space-x-5 space-y-5 pt-80  mt-60 md:pt-80 md:mt-10 lg:pt-60  ">
+    <button>
+    </button>
+
+  </Link>
+Link>
+</div>
+<div className="space-x-5 space-y-5 pt-80  mt-60 md:pt-80 md:mt-10 lg:pt-60  ">
+    <button>
+    </button>
+
+  </Link>
+Link>
+</div>
 
 <div className="space-x-5 space-y-5 pt-80  mt-60 md:pt-80 md:mt-10 lg:pt-60  ">
     <button>
